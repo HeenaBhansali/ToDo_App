a103ac9cff5fdbb4e7371ff66884a17321b8d136
Heena <heenaj198@gmail.com>
2019-08-06 17:15:25 +0530

    Add and Delete working

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -1,3 +1,5 @@
+var ul = document.getElementById('list')
+
 document.getElementById('add').addEventListener('click', function() {
     var value = document.getElementById('text').value
     console.log(value)
@@ -7,9 +9,29 @@ document.getElementById('add').addEventListener('click', function() {
 })
 
 function addItem(item) {
-    var node = document.createElement('p')
-        //var textnode = document.createTextNode(item)
-        //node.appendChild(textnode)
-    node.innerHTML = item
-    document.querySelector('.item').appendChild(node)
+    var node = document.createElement('li')
+    var textNode = document.createTextNode(item)
+
+    var edit = document.createElement('button')
+    edit.innerHTML = 'Edit'
+    edit.setAttribute('class', 'edit')
+    edit.addEventListener('click', function() {})
+
+    var remove = document.createElement('button')
+    remove.innerHTML = 'Delete'
+    remove.setAttribute('class', 'remove')
+    remove.addEventListener('click', function(e) {
+        // console.log(e.target.parentNode)
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode)
+    })
+
+    var radioInput = document.createElement('input')
+    radioInput.setAttribute('type', 'radio')
+    radioInput.setAttribute('class', 'complete')
+        //   var complete = document.createElement('')
+    document.getElementById('text').value = ''
+    node.append(radioInput)
+    node.appendChild(textNode)
+    node.append(remove, edit)
+    ul.appendChild(node)
 }
