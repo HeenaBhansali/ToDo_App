3520da1a8764a71d8912b6487139c818c30ea75d
Heena <heenaj198@gmail.com>
2019-08-09 12:51:40 +0530

    added local storage

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -1,88 +1,130 @@
-var ul = document.getElementById('list')
-document.getElementById('text').focus()
-var add = function() {
-    var value = document.getElementById('text').value.trim()
-    console.log(value)
-    if (value !== '') {
-        addItem(value)
+// const liElements = document.getElementsByTagName('li')
+const ul = document.getElementById('list')
+    // const btn = document.getElementById('add')
+
+// const add = function() {
+//   const value = document.getElementById('text').value.trim()
+//   console.log(value)
+//   if (value !== '') {
+//       addItem(value)
+//   }
+// }
+
+const add = function() {
+    const li = document.createElement('li')
+    const liText = document.getElementById('text').value
+    const textSpan = document.createElement('span')
+    const text = document.createTextNode(liText)
+    textSpan.appendChild(text)
+    textSpan.setAttribute('class', 'textSpan')
+    textSpan.setAttribute('id', 'lispan')
+    if (liText === '') {
+        alert('Enter your task!')
+    } else {
+        complete(li)
+        li.appendChild(textSpan)
+        editAndDel(li)
+        ul.appendChild(li)
+        document.getElementById('text').value = ''
+        setlocalStorage()
     }
 }
+const complete = function(elem) {
+    const completeBtn = document.createElement('button')
+    const complete = document.createTextNode('')
+    completeBtn.setAttribute('class', 'complete')
+    completeBtn.appendChild(complete)
+    elem.appendChild(completeBtn)
+    completeBtn.addEventListener('click', function(e) {
+        // console.log(e.target.parentNode)
+        const textSpan = e.target.parentNode.querySelector('span')
+        console.log(textSpan)
+        if (textSpan.className === 'inactive') {
+            completeBtn.innerHTML = ''
+            textSpan.setAttribute('class', 'active')
+        } else {
+            completeBtn.innerHTML = '\u2714'
+            textSpan.setAttribute('class', 'inactive')
+        }
+    })
+}
+const editAndDel = function(elem) {
+    const delBtn = document.createElement('button')
+    const editBtn = document.createElement('button')
+    const del = document.createTextNode('delete')
+    const edit = document.createTextNode('edit')
+    delBtn.setAttribute('class', 'delete')
+    editBtn.setAttribute('class', 'edit')
+        // delBtn.className = 'delete'
+        // editBtn.className = 'editaa'
+    delBtn.appendChild(del)
+    editBtn.appendChild(edit)
+    elem.appendChild(delBtn)
+    elem.appendChild(editBtn)
+}
+const deleter = function(e) {
+    if (e.classList.contains('delete')) {
+        // console.log(e)
+        e.parentNode.parentNode.removeChild(e.parentNode)
+        setlocalStorage()
+    }
+}
+
+const editor = function(e) {
+    if (e.classList.contains('edit')) {
+        const editContent = e.parentNode.querySelector('span')
+        editContent.setAttribute('id', 'editable')
+        editContent.contentEditable = true
+        editContent.focus()
+        const editmode = function(e) {
+            editContent.contentEditable = false
+            editContent.blur()
+            editContent.setAttribute('id', 'lispan')
+        }
+        document.addEventListener('click', function(e) {
+            if (e.target.parentNode.querySelector('span').id !== 'editable') editmode()
+
+            // if (e.target.id !== 'editable') editmode()
+        })
+        editContent.addEventListener('keypress', function(e) {
+                if (e.code === 'Enter') editmode()
+            })
+            // const textValue = elem.parentElement.firstChild.innerText
+            // const newValue = prompt('Editing', textValue)
+            // elem.parentElement.firstChild.innerText = newValue || textValue
+            // elem.parentElement.classList.remove('checked')
+    }
+    setlocalStorage()
+}
+
+const checkClick = function(func) {
+    console.log()
+    ul.addEventListener('click', function(event) {
+        const target = event.target
+        func(target)
+    })
+}
+
+const setlocalStorage = function() {
+    const str = JSON.stringify(ul.innerHTML)
+        // console.log('set' + str)
+    localStorage.setItem('todos', str)
+}
+
+const getlocalStorage = function() {
+    const str = localStorage.getItem('todos')
+    console.log('get' + str)
+    ul.innerHTML = JSON.parse(str)
+}
+
+getlocalStorage()
 document.getElementById('add').addEventListener('click', add)
 document.getElementById('text').addEventListener('keypress', function(e) {
     if (e.code === 'Enter') add()
 })
 
-function addItem(item) {
-    var node = document.createElement('li')
-    var snode = document.createElement('span')
-    var textNode = document.createTextNode(item)
-    snode.setAttribute('id', 'snode')
-    snode.setAttribute('class', 'snode')
-
-    var edit = document.createElement('button')
-    edit.innerHTML = 'Edit'
-    edit.setAttribute('class', 'edit')
-
-    edit.addEventListener('click', function(e) {
-        // console.log(e.target.parentNode)
-        // document.querySelectorAll('.complete').disabled = true
-        // e.target.parentNode.parentNode.querySelector('.remove').disabled = true
-
-        var sp = e.target.parentNode.querySelector('span')
-        sp.contentEditable = true
-        sp.setAttribute('class', 'active')
-        sp.focus()
-            // document.getElementById('snode').focus()
-
-        // var sp = document.getElementById('snode')
-        console.log(sp)
-        var editmode = function(e) {
-            sp.contentEditable = false *
-                snode.setAttribute('class', 'snode')
-        }
-        sp.addEventListener('mouseout', editmode)
-        sp.addEventListener('keypress', function(e) {
-            if (e.code === 'Enter') editmode()
-        })
-
-        // console.log(sp)
-        // console.log(e.target.parentNode.firstChild)
-        // e.target.parentNode.secondChild.contentEditable = true
-        // e.parentNode().attr('contenteditable', 'true')
-
-        // e.parent().attr('contenteditable', 'true')
-    })
-
-    var remove = document.createElement('button')
-    remove.innerHTML = 'Delete'
-    remove.setAttribute('class', 'remove')
-    remove.addEventListener('click', function(e) {
-        console.log(e.target.parentNode.parentNode)
-        e.target.parentNode.parentNode.removeChild(e.target.parentNode)
-    })
-
-    var complete = document.createElement('button')
-        // complete.innerHTML = '\u2714'
-        // complete.setAttribute('type', 'checkbox')
-    complete.setAttribute('class', 'complete')
-    complete.addEventListener('click', function(e) {
-        if (snode.className === 'inactive') {
-            complete.innerHTML = ''
-            snode.setAttribute('class', 'active')
-        } else {
-            complete.innerHTML = '\u2714'
-            snode.setAttribute('class', 'inactive')
-        }
-        // e.target.parentNode.setAttribute('class', 'inactive')
-    })
-
-    //   var complete = document.createElement('')
-    document.getElementById('text').value = ''
-    node.append(complete)
-    snode.appendChild(textNode)
-    node.append(snode)
-    node.append(remove, edit)
-        // node.setAttribute('class', 'active')
-
-    ul.appendChild(node)
-}
+// btn.onclick = createElement
+// checkClick(checkToggle)
+checkClick(deleter)
+checkClick(editor)
+    // checkClick(complete)
