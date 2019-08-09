f67b307a38543bd6a678468a840015fd5977fdaa
Heena <heenaj198@gmail.com>
2019-08-09 13:16:36 +0530

    updated local storage

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -1,14 +1,4 @@
-// const liElements = document.getElementsByTagName('li')
 const ul = document.getElementById('list')
-    // const btn = document.getElementById('add')
-
-// const add = function() {
-//   const value = document.getElementById('text').value.trim()
-//   console.log(value)
-//   if (value !== '') {
-//       addItem(value)
-//   }
-// }
 
 const add = function() {
     const li = document.createElement('li')
@@ -23,7 +13,9 @@ const add = function() {
     } else {
         complete(li)
         li.appendChild(textSpan)
-        editAndDel(li)
+
+        delfunc(li)
+        editfunc(li)
         ul.appendChild(li)
         document.getElementById('text').value = ''
         setlocalStorage()
@@ -36,7 +28,6 @@ const complete = function(elem) {
     completeBtn.appendChild(complete)
     elem.appendChild(completeBtn)
     completeBtn.addEventListener('click', function(e) {
-        // console.log(e.target.parentNode)
         const textSpan = e.target.parentNode.querySelector('span')
         console.log(textSpan)
         if (textSpan.className === 'inactive') {
@@ -48,34 +39,30 @@ const complete = function(elem) {
         }
     })
 }
-const editAndDel = function(elem) {
+const delfunc = function(elem) {
     const delBtn = document.createElement('button')
-    const editBtn = document.createElement('button')
     const del = document.createTextNode('delete')
-    const edit = document.createTextNode('edit')
     delBtn.setAttribute('class', 'delete')
-    editBtn.setAttribute('class', 'edit')
-        // delBtn.className = 'delete'
-        // editBtn.className = 'editaa'
     delBtn.appendChild(del)
-    editBtn.appendChild(edit)
     elem.appendChild(delBtn)
-    elem.appendChild(editBtn)
-}
-const deleter = function(e) {
-    if (e.classList.contains('delete')) {
-        // console.log(e)
-        e.parentNode.parentNode.removeChild(e.parentNode)
+    delBtn.addEventListener('click', function(e) {
+        console.log(e.target.parentNode)
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode)
         setlocalStorage()
-    }
+    })
 }
-
-const editor = function(e) {
-    if (e.classList.contains('edit')) {
-        const editContent = e.parentNode.querySelector('span')
+const editfunc = function(elem) {
+    const editBtn = document.createElement('button')
+    const edit = document.createTextNode('edit')
+    editBtn.setAttribute('class', 'edit')
+    editBtn.appendChild(edit)
+    elem.appendChild(editBtn)
+    editBtn.addEventListener('click', function(e) {
+        const editContent = e.target.parentNode.querySelector('span')
         editContent.setAttribute('id', 'editable')
         editContent.contentEditable = true
         editContent.focus()
+
         const editmode = function(e) {
             editContent.contentEditable = false
             editContent.blur()
@@ -83,37 +70,21 @@ const editor = function(e) {
         }
         document.addEventListener('click', function(e) {
             if (e.target.parentNode.querySelector('span').id !== 'editable') editmode()
-
-            // if (e.target.id !== 'editable') editmode()
         })
         editContent.addEventListener('keypress', function(e) {
-                if (e.code === 'Enter') editmode()
-            })
-            // const textValue = elem.parentElement.firstChild.innerText
-            // const newValue = prompt('Editing', textValue)
-            // elem.parentElement.firstChild.innerText = newValue || textValue
-            // elem.parentElement.classList.remove('checked')
-    }
-    setlocalStorage()
-}
-
-const checkClick = function(func) {
-    console.log()
-    ul.addEventListener('click', function(event) {
-        const target = event.target
-        func(target)
+            if (e.code === 'Enter') editmode()
+        })
+        setlocalStorage()
     })
 }
 
 const setlocalStorage = function() {
     const str = JSON.stringify(ul.innerHTML)
-        // console.log('set' + str)
     localStorage.setItem('todos', str)
 }
 
 const getlocalStorage = function() {
     const str = localStorage.getItem('todos')
-    console.log('get' + str)
     ul.innerHTML = JSON.parse(str)
 }
 
@@ -122,9 +93,3 @@ document.getElementById('add').addEventListener('click', add)
 document.getElementById('text').addEventListener('keypress', function(e) {
     if (e.code === 'Enter') add()
 })
-
-// btn.onclick = createElement
-// checkClick(checkToggle)
-checkClick(deleter)
-checkClick(editor)
-    // checkClick(complete)
