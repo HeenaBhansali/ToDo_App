6f53bb7e3bc060839c49ae31dfd074606bd3f4a6
Heena <heenaj198@gmail.com>
2019-08-28 09:00:55 +0530

    rewriting javascript code

--- ToDo_App/script.js
+++ ToDo_App/script.js
@@ -1,135 +1,158 @@
-// const liElements = document.getElementsByTagName('li')
-const ul = document.getElementById('list')
-    // const btn = document.getElementById('add')
+const div = document.getElementById('item')
+const button = document.getElementById('add')
+const input = document.getElementById('text')
 
-// const add = function() {
-//   const value = document.getElementById('text').value.trim()
-//   console.log(value)
-//   if (value !== '') {
-//       addItem(value)
+let itemsArray
+
+if (localStorage.getItem('items')) {
+    itemsArray = JSON.parse(localStorage.getItem('items'))
+} else {
+    itemsArray = []
+}
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+const divMaker = text => {
+    const divitem = document.createElement('div')
+    divitem.textContent = text
+    div.appendChild(divitem)
+}
+button.addEventListener('click', function(e) {
+    let count = itemsArray.length + 1
+    let val = input.value
+    let obj = {
+        id: count,
+        value: val
+    }
+
+    itemsArray.push(obj)
+        //   console.log(obj.val)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    divMaker(input.value)
+    input.value = ''
+})
+
+data.forEach(item => {
+    divMaker(item)
+})
+
+// const ul = document.getElementById('list')
+
+// const add = function () {
+//   const li = document.createElement('li')
+//   const liText = document.getElementById('text').value.trim()
+//   const textSpan = document.createElement('span')
+//   const text = document.createTextNode(liText)
+//   textSpan.appendChild(text)
+//   textSpan.setAttribute('class', 'textSpan')
+//   textSpan.setAttribute('id', 'lispan')
+//   if (liText === '') {
+//     alert('Enter your task!')
+//   } else {
+//     complete(li)
+//     li.appendChild(textSpan)
+//     notesEditDel(li)
+//     ul.appendChild(li)
+//     document.getElementById('text').value = ''
+//     setlocalStorage()
+//   }
+// }
+// const complete = function (e) {
+//   const completeBtn = document.createElement('button')
+//   const complete = document.createTextNode('')
+//   completeBtn.setAttribute('class', 'complete')
+//   completeBtn.appendChild(complete)
+//   e.appendChild(completeBtn)
+// }
+// const compFunc = function (e) {
+//   if (e.classList.contains('complete')) {
+//     const textSpan = e.parentNode.querySelector('span')
+//     console.log(textSpan)
+//     if (textSpan.className === 'inactive') {
+//       e.parentNode.firstChild.innerHTML = ''
+//       textSpan.seitemsArraytAttribute('class', 'active')
+//     } else {
+//       e.parentNode.firstChild.innerHTML = '\u2714'
+//       textSpan.setAttribute('class', 'inactive')
+//     }
+//   }
+//   setlocalStorage()
+// }
+
+// const notesEditDel = function (e) {
+//   const notesBtn = document.createElement('button')
+//   const delBtn = document.createElement('button')
+//   const editBtn = document.createElement('button')
+//   const notes = document.createTextNode('notes')
+//   const del = document.createTextNode('delete')
+//   const edit = document.createTextNode('edit')
+//   notesBtn.setAttribute('class', 'notes')
+//   delBtn.setAttribute('class', 'delete')
+//   editBtn.setAttribute('class', 'edit')
+//   notesBtn.appendChild(notes)
+//   delBtn.appendChild(del)
+//   editBtn.appendChild(edit)
+
+//   e.appendChild(delBtn)
+//   e.appendChild(editBtn)
+//   //   e.appendChild(notesBtn)
+// }
+
+// const delFunc = function (e) {
+//   if (e.classList.contains('delete')) {
+//     console.log(e.target)
+//     e.parentNode.parentNode.removeChild(e.parentNode)
+//     setlocalStorage()
 //   }
 // }
 
-const add = function() {
-    const li = document.createElement('li')
-    const liText = document.getElementById('text').value
-    const textSpan = document.createElement('span')
-    const text = document.createTextNode(liText)
-    textSpan.appendChild(text)
-    textSpan.setAttribute('class', 'textSpan')
-    textSpan.setAttribute('id', 'lispan')
-    if (liText === '') {
-        alert('Enter your task!')
-    } else {
-        complete(li)
-        li.appendChild(textSpan)
-        notesEditDel(li)
-        ul.appendChild(li)
-        document.getElementById('text').value = ''
-        setlocalStorage()
-    }
-}
-const complete = function(elem) {
-    const completeBtn = document.createElement('button')
-    const complete = document.createTextNode('')
-    completeBtn.setAttribute('class', 'complete')
-    completeBtn.appendChild(complete)
-    elem.appendChild(completeBtn)
-}
-const taskDone = function(e) {
-    if (e.classList.contains('complete')) {
-        // console.log(e.target.parentNode)
-        const textSpan = e.parentNode.querySelector('span')
-        console.log(textSpan)
-        if (textSpan.className === 'inactive') {
-            e.parentNode.firstChild.innerHTML = ''
-            textSpan.setAttribute('class', 'active')
-        } else {
-            e.parentNode.firstChild.innerHTML = '\u2714'
-            textSpan.setAttribute('class', 'inactive')
-        }
-    }
-    setlocalStorage()
-}
+// const editFunc = function (e) {
+//   if (e.classList.contains('edit')) {
+//     const editContent = e.parentNode.querySelector('span')
+//     editContent.setAttribute('id', 'editable')
+//     editContent.contentEditable = true
+//     editContent.focus()
+//     const editmode = function (e) {
+//       editContent.contentEditable = false
+//       editContent.blur()
+//       editContent.setAttribute('id', 'lispan')
+//       setlocalStorage()
+//     }
+//     document.addEventListener('click', function (e) {
+//       if (e.target.parentNode.querySelector('span').id !== 'editable') editmode()
+//     })
+//     editContent.addEventListener('keypress', function (e) {
+//       if (e.code === 'Enter') editmode()
+//     })
+//   }
+//   setlocalStorage()
+// }
 
-const notesEditDel = function(elem) {
-        const notesBtn = document.createElement('button')
-        const delBtn = document.createElement('button')
-        const editBtn = document.createElement('button')
-        const notes = document.createTextNode('notes')
-        const del = document.createTextNode('delete')
-        const edit = document.createTextNode('edit')
-        notesBtn.setAttribute('class', 'notes')
-        delBtn.setAttribute('class', 'delete')
-        editBtn.setAttribute('class', 'edit')
-        notesBtn.appendChild(notes)
-        delBtn.appendChild(del)
-        editBtn.appendChild(edit)
+// const checkClick = function (func) {
+//   ul.addEventListener('click', function (e) {
+//     const target = e.target
+//     console.log(target)
+//     func(target)
+//   })
+// }
 
-        elem.appendChild(delBtn)
-        elem.appendChild(editBtn)
-            //   elem.appendChild(notesBtn)
-    }
-    // const notes = function (e) {
-    //   if (e.classList.contains('notes')) {
-    //     console.log(e)
-    //   }
-    // }
-const deleter = function(e) {
-    if (e.classList.contains('delete')) {
-        e.parentNode.parentNode.removeChild(e.parentNode)
-        setlocalStorage()
-    }
-}
+// const setlocalStorage = function () {
+//   const str = JSON.stringify(ul.innerHTML)
+//   localStorage.setItem('todos', str)
+// }
 
-const editor = function(e) {
-    if (e.classList.contains('edit')) {
-        const editContent = e.parentNode.querySelector('span')
-        editContent.setAttribute('id', 'editable')
-        editContent.contentEditable = true
-        editContent.focus()
-        const editmode = function(e) {
-            editContent.contentEditable = false
-            editContent.blur()
-            editContent.setAttribute('id', 'lispan')
-            setlocalStorage()
-        }
-        document.addEventListener('click', function(e) {
-            if (e.target.parentNode.querySelector('span').id !== 'editable') editmode()
+// const getlocalStorage = function () {
+//   const str = localStorage.getItem('todos')
+//   ul.innerHTML = JSON.parse(str)
+// }
 
-            // if (e.target.id !== 'editable') editmode()
-        })
-        editContent.addEventListener('keypress', function(e) {
-            if (e.code === 'Enter') editmode()
-        })
-    }
-    setlocalStorage()
-}
+// getlocalStorage()
+// document.getElementById('add').addEventListener('click', add)
+// document.getElementById('text').addEventListener('keypress', function (e) {
+//   if (e.code === 'Enter') add()
+// })
 
-const checkClick = function(func) {
-    ul.addEventListener('click', function(event) {
-        const target = event.target
-        func(target)
-    })
-}
-
-const setlocalStorage = function() {
-    const str = JSON.stringify(ul.innerHTML)
-    localStorage.setItem('todos', str)
-}
-
-const getlocalStorage = function() {
-    const str = localStorage.getItem('todos')
-    ul.innerHTML = JSON.parse(str)
-}
-
-getlocalStorage()
-document.getElementById('add').addEventListener('click', add)
-document.getElementById('text').addEventListener('keypress', function(e) {
-    if (e.code === 'Enter') add()
-})
-
-checkClick(deleter)
-checkClick(editor)
-checkClick(taskDone)
-    // checkClick(notes)
+// checkClick(delFunc)
+// checkClick(editFunc)
+// checkClick(compFunc)
